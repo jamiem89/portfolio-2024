f2cbf9f967a13a669d93252c64c2cc32df3b20a2
Jamie Moore <jamiemoore2689@gmail.com>
2023-12-23 16:34:24 +0000

    fleshed out most of the homepage

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,31 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'deep-purple':'rgba(15, 15, 31, 1)',
+				'bright-purple': 'rgba(92, 22, 247, 1)',
+				'electric-blue': 'rgba(15, 22, 255, 1)',
+				'black': 'rgba(51, 51, 51, 1)',
+			},
+			fontSize: {
+				'h2': ['28px', {
+					lineHeight: '1.125em',
+					fontWeight: '800',
+				}],
+				'h2Med': ['42px', {
+					fontWeight: '800',
+				}],
+				'h3': ['24px', {
+					lineHeight: '1.125em',
+					fontWeight: '800',
+				}],
+				'h3Med': ['32px', {
+					fontWeight: '800',
+				}],
+				'tiny': '10px',
+			}
+		},
 	},
 	plugins: [],
 }
